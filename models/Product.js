65f6cb951fe054ba8f2bbba160050cc05f569022
Mattchu <MGPIRMAL678@GMAIL.COM>
2021-03-16 20:15:25 -0400

    models completed

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, DECIMAL } = require('sequelize');
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection').default;
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,6 +10,43 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validate if decimal
+      validate: {
+        isDecimal: true,
+      },
+      },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // default 10
+      defaultValue: 10,
+      // validate numeric
+      validate: {
+        isNumeric: true,
+      
+      },
+
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
